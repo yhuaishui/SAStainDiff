0101e7caa45d8327f1e5df5007f58c79e01603f4
yhuaishui <72019097+yhuaishui@users.noreply.github.com>
2025-03-27 12:29:57 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 # use_anysize: Restore Any-size Image sampling strategy
 # use_ddim: DDIM sampling strategy
 
-SAMPLE_FLAGS="--input_path ./data/test/ --save_path ./data/sample/ --model_path ./checkpoint/other_to_Aperio.pt --use_ddim True --timestep_step 850 --timestep_respacing ddim5 --use_anysize False"
+SAMPLE_FLAGS="--input_path ./data/test/ --save_path ./data/sample/ --model_path ./checkpoint/other_to_Aperio.pt --use_ddim True --timestep_step 900 --timestep_respacing ddim5 --use_anysize False"
 
 python test.py $SAMPLE_FLAGS
 
